030a32e1d4f81cdaed057bdad36391a6554f53c1
k88hudson <k88hudson@gmail.com>
2018-03-07 11:32:58 -0500

    added sandbox stuff

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,27 @@
 const {fetchQuery} = require("./lib/queryUtils");
 const AS_COMPONENTS = ["Activity Streams: Newtab", "New Tab Page", "Activity Streams: Application Servers"];
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
 
 async function main() {
-  const results = await fetchQuery({
+  const query = {
     component: AS_COMPONENTS,
-    include_fields: ["id", "summary", "severity"],
+    include_fields: ["id", "summary"],
     iteration: 60.4,
     custom: {
       blocked: 1432662
     }
-  });
+  };
+  const results = await fetchQuery(query);
+  if (!results || !results.length) {
+    console.log("No results were found.");
+    return;
+  }
   console.log(results);
+  const filename = `sandbox_results/${Date.now()}_RESULTS.json`;
+  fs.writeFileSync(path.join(__dirname, filename), JSON.stringify({query,results}, null, 2));
+  console.log(chalk.green(`\nSuccess: ${results.length} bugs found.`) + `\nWrote results to ${filename}`);
 }
 
 main();
