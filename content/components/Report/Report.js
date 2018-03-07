7fdb8b6679324b6cb15f45e020f6e1ec4cfd24eb
k88hudson <k88hudson@gmail.com>
2018-03-07 14:03:31 -0500

    style

--- content/components/Report/Report.js
+++ content/components/Report/Report.js
@@ -8,15 +8,34 @@ const DEFAULTS = {
 };
 
 export class Report extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   async componentDidMount() {
-    const bugsFiled = await runQuery({
-
-    })
+    const currentIterationBugs = await runQuery({
+      include_fields: ["id", "status"],
+      component: DEFAULTS.component,
+      iteration: 60.4
+    });
+    this.setState({currentIterationBugs});
+  }
+  renderCurrentIterationStats(bugs) {
+    if (!bugs || !bugs.length) return;
+    return <React.Fragment>
+      <tr><td>Total bugs</td><td>{bugs.length}</td></tr>
+      <tr><td>Completed</td><td>{bugs.filter(b => b.status === "RESOLVED").length}</td></tr>
+    </React.Fragment>
   }
   render() {
-    const {props} = this;
+    const {prop, state} = this;
     return (<div className={styles.container}>
       <h2>Report</h2>
+      <table className={styles.table}>
+        <tbody>
+        {this.renderCurrentIterationStats(state.currentIterationBugs)}
+        </tbody>
+      </table>
     </div>);
   }
 }
