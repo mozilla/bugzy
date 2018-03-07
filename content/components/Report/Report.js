b958c8a85566f5d8d1ff66e3021764297b0e9b07
k88hudson <k88hudson@gmail.com>
2018-03-07 13:31:14 -0500

    style

--- content/components/Report/Report.js
+++ content/components/Report/Report.js
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "./Report.scss";
 import {runQuery, AS_COMPONENTS} from "../../lib/utils";
 
+const DEFAULTS = {
+  component: ["Activity Streams: Newtab", "New Tab Page", "Activity Streams: Application Servers"],
+  include_fields: ["id","summary", "status", "assigned_to", "priority"]
+};
+
 export class Report extends React.PureComponent {
+  async componentDidMount() {
+    const bugsFiled = await runQuery({
+
+    })
+  }
   render() {
     const {props} = this;
     return (<div className={styles.container}>
