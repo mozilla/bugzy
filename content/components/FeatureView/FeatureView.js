0bca15075637f396c1e1b44e4df3894346bdd9c8
k88hudson <k88hudson@gmail.com>
2018-05-16 17:33:46 -0400

    added file new bug link to feature pages

--- content/components/FeatureView/FeatureView.js
+++ content/components/FeatureView/FeatureView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./FeatureView.scss";
+import gStyles from "../../styles/gStyles.scss";
 import {BugList} from "../BugList/BugList";
 import {Loader} from "../Loader/Loader";
 import {runQuery, isBugResolved} from "../../lib/utils";
@@ -76,6 +77,10 @@ export class FeatureView extends React.PureComponent {
   componentWillMount() {
     this.getBugs(this.props.match.params.id);
   }
+  renderFileNewBug(bugNumber) {
+    const url = `https://bugzilla.mozilla.org/enter_bug.cgi?blocked=${bugNumber}&product=Firefox&component=Activity Streams%3A Newtab`;
+    return <a className={gStyles.primaryButton + " " + styles.headerButton} href={url}>File new bug</a>
+  }
   renderBugs(bugs) {
     const bugsByRelease = this.sortByRelease(this.state.bugs);
     return <React.Fragment>
@@ -95,7 +100,8 @@ export class FeatureView extends React.PureComponent {
 
     const metaId = this.props.match.params.id;
     return (<div className={styles.container}>
-      <h1><a href={"https://bugzilla.mozilla.org/show_bug.cgi?id=" + metaId}>{metasById[metaId].displayName} ({metaId})</a></h1>
+      <h1><a href={"https://bugzilla.mozilla.org/show_bug.cgi?id=" + metaId}>{metasById[metaId].displayName}</a> {this.renderFileNewBug(metaId)}</h1>
+      <p className={styles.subheading}>This list includes bugs in any component blocking meta bug <a href={"https://bugzilla.mozilla.org/show_bug.cgi?id=" + metaId}> {metaId}</a>.</p>
       {this.state.loaded ? this.renderBugs() : <Loader />}
     </div>);
   }
