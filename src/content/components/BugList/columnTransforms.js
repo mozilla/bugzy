mozilla/bugzy

commit 08b81016d86abcfd204f3b2262e624c02ef45289

Fix #28: Add 'chore' tags

src/content/components/BugList/columnTransforms.js
@@ -6,6 +6,8 @@ import {DateTime} from "luxon";
 
 const OPEN_BUG_URL = "https://bugzilla.mozilla.org/show_bug.cgi?id=";
 
+const WHITEBOARD_TAGS = ["needinfo", "chore"];
+
 const numberWithSpaces = n => {
   const letters = n.toString().split("");
   return (<React.Fragment><span>{letters.slice(0, -3).join("")}</span><span>{letters.slice(-3).join("")}</span></React.Fragment>);
@@ -33,8 +35,10 @@ function renderWhiteboard({whiteboard, keywords, severity, hasPR, flags}) {
   if (hasPR) {
     tags.push("has-pr");
   }
-  if (flags && flags.find(flag => flag.name === "needinfo")) {
-    tags.push("NEEDINFO");
+  if (flags) {
+    WHITEBOARD_TAGS
+      .filter(tag => flags.find(flag => flag.name === tag))
+      .forEach(tags.push)
   }
 
   return <ul className={styles.tagList}>{tags.map(tag => {

src/config/tags.js
@@ -5,7 +5,7 @@ module.exports = {
     style: {color: "var(--red-50)", fontWeight: "bold"},
     label: "defect"
   },
-  "NEEDINFO": {
+  "needinfo": {
     style: {color: "var(--gray-90)", fontWeight: "bold"},
     label: "🤔 needinfo"
   },
@@ -32,5 +32,9 @@ module.exports = {
   "strings m-c needed": {
     label: "strings m-c needed",
     style: {backgroundColor: "var(--teal-80", color: "white", border: 0, fontWeight: "bold"}
+  },
+  "chore": {
+    label: "chore",
+    style: {backgroundColor: "var(--purple-60)", color: "white", border: 0, fontWeight: "bold"}
   }
 };

commit c2a75fd9c7cc19bc37b5db3c6b4f5f5b613011a0

Fix bug introduced in 08b81016

src/content/components/BugList/columnTransforms.js
@@ -6,8 +6,6 @@ import {DateTime} from "luxon";
 
 const OPEN_BUG_URL = "https://bugzilla.mozilla.org/show_bug.cgi?id=";
 
-const WHITEBOARD_TAGS = ["needinfo", "chore"];
-
 const numberWithSpaces = n => {
   const letters = n.toString().split("");
   return (<React.Fragment><span>{letters.slice(0, -3).join("")}</span><span>{letters.slice(-3).join("")}</span></React.Fragment>);
@@ -35,10 +33,8 @@ function renderWhiteboard({whiteboard, keywords, severity, hasPR, flags}) {
   if (hasPR) {
     tags.push("has-pr");
   }
-  if (flags) {
-    WHITEBOARD_TAGS
-      .filter(tag => flags.find(flag => flag.name === tag))
-      .forEach(tags.push)
+  if (flags && flags.find(flag => flag.name === "needinfo")) {
+    tags.push("needinfo");
   }
 
   return <ul className={styles.tagList}>{tags.map(tag => {