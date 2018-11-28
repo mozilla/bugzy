4d79228df7301802a429674d60ec99ea216fce05
k88hudson <k88hudson@gmail.com>
2018-11-28 16:16:42 -0500

    uplift tracking improvements

--- src/content/components/Uplift/Uplift.js
+++ src/content/components/Uplift/Uplift.js
@@ -34,9 +34,20 @@ export class Uplift extends React.PureComponent {
     const {bugs: upliftRequested} = await runQuery(getFlagQuery("?"));
     const {bugs: upliftDenied} = await runQuery(getFlagQuery("-"));
     const {bugs: upliftComplete} = await runQuery(getFlagQuery("+"));
+    const betakey = `cf_tracking_firefox${prevRelease}`;
+    const {bugs: tracking} = await runQuery({
+      include_fields: columns.concat([trackingField, statusField, statusNightly]),
+      component: BUGZILLA_TRIAGE_COMPONENTS,
+      order: "changeddate DESC",
+      custom: {
+        [betakey]: {anyexact: ["?", "+"]},
+        "flagtypes.name": {notsubstring: `approval-mozilla-beta`}
+      }
+    });
     this.setState({
       loaded: true,
       bugs: {
+        tracking,
         upliftRequested,
         upliftApproved: upliftComplete.filter(b => b.cf_tracking_beta === "+" && !(["verified", "fixed"].includes(b.cf_status_beta))),
         upliftDenied: upliftDenied.filter(b => b.cf_tracking_beta === "+" && !(["verified", "fixed"].includes(b.cf_status_beta))),
@@ -48,13 +59,20 @@ export class Uplift extends React.PureComponent {
   renderContent() {
     const displayColumns = [...columns, "cf_status_nightly", "cf_status_beta"];
     return (<React.Fragment>
+      <h2>Tracking for uplift with no patch yet</h2>
+      <p>These bugs have been flagged for uplift but do not have a request open on a patch.</p>
+      <BugList bulkEdit={true} tags={true} bugs={this.state.bugs.tracking} columns={displayColumns} />
       <h2>Requested</h2>
+      <p>These bugs have a request for uplift open on a patch.</p>
       <BugList bulkEdit={true} tags={true} bugs={this.state.bugs.upliftRequested} columns={displayColumns} />
       <h2>Denied</h2>
+      <p>These bugs have a patch that was denied for uplift.</p>
       <BugList bulkEdit={true} tags={true} bugs={this.state.bugs.upliftDenied} columns={displayColumns} />
       <h2>Approved</h2>
+      <p>These bugs have a patch that was approved for uplift.</p>
       <BugList bulkEdit={true} tags={true} bugs={this.state.bugs.upliftApproved} columns={displayColumns} />
       <h2>Landed</h2>
+      <p>These bugs have been merged to beta.</p>
       <BugList bulkEdit={true} tags={true} bugs={this.state.bugs.upliftComplete} columns={displayColumns} />
     </React.Fragment>);
   }
