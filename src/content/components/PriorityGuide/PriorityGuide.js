5ac09322a0c06534275ab8dd2b95c75fc8f6e932
k88hudson <k88hudson@gmail.com>
2019-02-20 15:41:02 -0500

    P1/P2 definition changes andr related query changes

--- src/content/components/PriorityGuide/PriorityGuide.js
+++ src/content/components/PriorityGuide/PriorityGuide.js
@@ -6,13 +6,13 @@ export class PriorityGuide extends React.PureComponent {
     return (<div className={styles.container}>
       <h2>Priority Guide</h2>
 
-      <p><strong className={styles.p1}>P1</strong> <strong>In progress</strong> in the current iteration, assigned, and currently being worked on.</p>
+      <p><strong className={styles.p1}>P1</strong> <strong>Current release</strong> required for the release currently in Firefox nightly or for uplift.</p>
 
-      <p><strong className={styles.p2}>P2</strong> <strong>Next up</strong> required for the current release, but not yet being worked on.</p>
+      <p><strong className={styles.p2}>P2</strong> <strong>Next up</strong> required for the next upcoming release.</p>
 
-      <p><strong className={styles.p3}>P3</strong> <strong>Backlog</strong> not required for the current release. Choose from this list if all P2s are completed.</p>
+      <p><strong className={styles.p3}>P3</strong> <strong>Backlog</strong> not required for the next two releases. Choose from this list if all other bugs are completed.</p>
 
-      <p><strong className={styles.p4}>P4</strong> <strong>Deprioritized</strong> valid but not important enough to be assigned to an iteration (although we would accept patches).</p>
+      <p><strong className={styles.p4}>P5</strong> <strong>Deprioritized</strong> valid but not important enough to be assigned to an iteration (although we would accept patches).</p>
     </div>);
   }
 }
