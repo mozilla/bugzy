b2f7498250bee29877957ecf79beaa27d6a9648b
Emily McMinn <emily@mcminn.ca>
2019-07-03 10:18:54 -0400

    interim changes for Typescript

--- src/content/components/IterationView/IterationView.tsx
+++ src/content/components/IterationView/IterationView.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BugList } from "../BugList/BugList";
-import { useBugFetcher } from "../../hooks/useBugFetcher";
+import { useBugFetcher, Bug } from "../../hooks/useBugFetcher";
 import { Container } from "../ui/Container/Container";
 import { getIteration } from "../../../common/iterationUtils";
 import { Tabs } from "../ui/Tabs/Tabs";
 
 interface GetQueryOptions {
   iteration: string;
-  metas: Array<{ id: string; component: string }>;
+  metas: Array<{
+    id: string;
+    component: string;
+    priority?: string;
+    displayName?: string;
+  }>;
   components: string[];
 }
 
@@ -48,17 +53,82 @@ const getQuery = (options: GetQueryOptions) => ({
   ],
 });
 
+interface BugProps {
+  id: any;
+  summary: string;
+  assigned_to?: string;
+  priority?: string;
+  status?: string;
+  whiteboard?: string;
+  keywords?: string;
+  type?: string;
+  flags?: string;
+  blocks?: string;
+  component: string;
+}
+
 function computeHeading(iteration: string): string {
   const currentIterationInformation = getIteration();
   const isCurrent = iteration === currentIterationInformation.number;
   return `${isCurrent ? "Current " : ""}Iteration (${iteration})`;
 }
 
+interface GetSortOptions {
+  metas: Array<{
+    id: string;
+    component?: string;
+    priority?: string;
+    displayName?: string;
+  }>;
+  bugs: any[];
+}
+
+function sortByMeta(
+  metas: GetSortOptions["metas"],
+  bugs: any[]
+): { [metaNumber: string]: Bug[] } {
+  let bugsByMeta = {};
+  // DO SOME SORTING YO
+  bugs.forEach(bug => {
+    if (!metas.length) {
+      metas.push({ id: "other", displayName: "Other" });
+    }
+
+    metas.forEach(meta => {
+      if (!bugsByMeta[meta.id]) {
+        bugsByMeta[meta.id] = { meta, bugs: [] };
+      }
+      bugsByMeta[meta.id].bugs.push(bug);
+    });
+  });
+  return bugsByMeta;
+}
+
+function renderBugList(meta: GetSortOptions["metas"], bugs: any[]) {
+  return (
+    <BugList
+      key={meta.id}
+      compact={true}
+      subtitle={meta.displayName}
+      tags={true}
+      bulkEdit={true}
+      showHeaderIfEmpty={true}
+      bugs={bugs}
+      bugzilla_email={this.state.bugzilla_email}
+    />
+  );
+}
+
 interface IterationViewProps {
   /* e.g. "65.4" */
   iteration: string;
   /* Metas for all bugzy stuff */
-  metas: Array<{ id: string; component: string }>;
+  metas: Array<{
+    id: string;
+    component: string;
+    priority?: string;
+    displayName?: string;
+  }>;
   match: { url: string };
 }
 
@@ -68,22 +138,16 @@ const IterationViewTab = props => {
   const state = useBugFetcher({
     query,
     updateOn: [],
-    transformBugs(bugs) {
-      // No transform for now
-      return bugs;
-    },
   });
-  return state.status === "loaded" ? (
-    <BugList
-      compact={true}
-      tags={true}
-      bulkEdit={true}
-      showHeaderIfEmpty={true}
-      bugs={state.bugs}
-    />
-  ) : (
-    "Loading..."
-  );
+
+  sortByMeta(this.props.metas, state.bugs);
+
+  return state.status === "loaded"
+    ? // ForEach meta....
+      Object.keys(state.bugsByMeta).map(id =>
+        renderBugList(state.bugsByMeta[id].meta, state.bugsByMeta[id].bugs)
+      )
+    : "Loading...";
 };
 
 export const IterationView: React.FunctionComponent<
@@ -101,6 +165,7 @@ export const IterationView: React.FunctionComponent<
               return (
                 <IterationViewTab
                   {...props}
+                  metas={this.props.metas}
                   components={["Messaging System"]}
                 />
               );
@@ -113,6 +178,7 @@ export const IterationView: React.FunctionComponent<
               return (
                 <IterationViewTab
                   {...props}
+                  metas={this.props.metas}
                   components={["Activity Streams: Newtab"]}
                 />
               );
