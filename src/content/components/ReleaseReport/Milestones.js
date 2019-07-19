cd0a53538a358c4f18892c9dc721d79f21fa6bf8
Emily McMinn <emily@mcminn.ca>
2019-07-19 14:49:46 -0400

    added more milestones to the date table

--- src/content/components/ReleaseReport/Milestones.js
+++ src/content/components/ReleaseReport/Milestones.js
@@ -1,9 +1,21 @@
 import { DateTime } from "luxon";
-import { getIteration } from "../../../common/iterationUtils";
+import { getIteration, getMondayBefore } from "../../../common/iterationUtils";
 import { ITERATION_LOOKUP } from "../../../common/ITERATION_LOOKUP";
 
 const iteration = getIteration();
 const release = iteration.number.split(".")[0];
+
+function getNextRelease(release) {
+  let nextRelease = parseInt(release) + 1;
+
+  return nextRelease;
+}
+
+function getPrevRelease() {
+  let prevRelease = parseInt(release) - 1;
+
+  return prevRelease;
+}
 
 function getStartDate(milestone) {
   let iterationDate = ITERATION_LOOKUP.byVersionString[milestone];
@@ -12,7 +24,27 @@ function getStartDate(milestone) {
   return milestoneDate;
 }
 
+function getEndDate(milestone) {
+  let iterationDate = ITERATION_LOOKUP.byVersionString[milestone];
+  let milestoneDate = DateTime.fromISO(iterationDate.endDate);
+
+  return milestoneDate;
+}
+
 export const RELEASE_MILESTONES = [
+  {
+    label: "Bug Breakdown",
+    calculate() {
+      let prevRelease = getPrevRelease(release);
+      return getEndDate(`${prevRelease}.4`).plus({ days: -2 });
+    },
+  },
+  {
+    label: "PI Request Due",
+    calculate() {
+      return getEndDate(`${release}.1`).plus({ days: -2 });
+    },
+  },
   {
     label: `${release}.1`,
     calculate() {
@@ -23,6 +55,12 @@ export const RELEASE_MILESTONES = [
     label: `${release}.2`,
     calculate() {
       return getStartDate(this.label);
+    },
+  },
+  {
+    label: "Tech Documentation Due",
+    calculate() {
+      return getEndDate(`${release}.2`).plus({ days: -2 });
     },
   },
   {
@@ -38,16 +76,29 @@ export const RELEASE_MILESTONES = [
     },
   },
   {
+    label: "Feature Complete",
+    calculate() {
+      return getEndDate(`${release}.4`).plus({ weeks: -1, days: -2 });
+    },
+  },
+  {
+    label: "Nightly Code Freeze",
+    calculate() {
+      let endDate = getEndDate(`${release}.4`);
+      return getMondayBefore(endDate);
+    },
+  },
+  {
     label: "Merge Day",
     calculate() {
-      const nextRelease = parseInt(release) + 1;
+      const nextRelease = getNextRelease(release);
       return getStartDate(`${nextRelease}.1`);
     },
   },
   {
     label: "Release Day",
     calculate() {
-      const nextRelease = parseInt(release) + 1;
+      const nextRelease = getNextRelease(release);
       return getStartDate(`${nextRelease}.1`).plus({ days: 1 });
     },
   },
