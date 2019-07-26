e3857eac9f859ec88195f7891f272d69c49b91d4
Kate Hudson <k88hudson@gmail.com>
2019-07-26 19:40:38 -0400

    Revert "DEPLOY"
    This reverts commit b8f40435e99dbeba63bc1b0254e39e8d4b14a536.

--- dist/common/removeMeta.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/** Removes the [meta]/[META] label from a bug title  */
-Object.defineProperty(exports, "__esModule", { value: true });
-const META_REGEX = /^\[\s*meta\s*\]\s*/i;
-const FX_VERSION_REGEX = /\s(in\s|-\s)*(firefox|fx)\s*\d+/i;
-function removeMeta(summary) {
-    return summary.replace(META_REGEX, "").replace(FX_VERSION_REGEX, "");
-}
-exports.removeMeta = removeMeta;
--- dist/config/pref_defaults.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // The user's bugzilla email
-    bugzilla_email: "",
-    offline_debug: false,
-};
