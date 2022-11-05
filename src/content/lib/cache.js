696ebfca42c088d632938744ffea06cede307e19
aminomancer <shughes@mozilla.com>
2022-11-04 22:07:10 -0700

    Move the fake request behavior to cache.js

--- src/content/lib/cache.js
+++ src/content/lib/cache.js
@@ -1,18 +1,14 @@
 import { prefs } from "./prefs";
 
-class Cache {
+class BugsCache {
   constructor({ version } = {}) {
-    if (!version) {
-      throw new Error("Cache version is required");
-    }
+    if (!version) throw new Error("Cache version is required");
     this._opened = false;
     this._version = version;
   }
 
   _open() {
-    if (prefs.get("disable_cache")) {
-      return null;
-    }
+    if (prefs.get("disable_cache")) return null;
     try {
       const cache = window.caches.open(this._version);
       if (!this._opened) {
@@ -29,7 +25,7 @@ class Cache {
   async get(request) {
     try {
       const cache = await this._open();
-      return cache && cache.match(request);
+      return cache.match(request.asGet);
     } catch (error) {
       console.debug("Error accessing cache :>> ", error);
       return null;
@@ -37,12 +33,10 @@ class Cache {
   }
 
   async set(request, response) {
-    if (!response) {
-      return false;
-    }
+    if (!response) return null;
     try {
       const cache = await this._open();
-      return cache && cache.put(request, response.clone());
+      return cache.put(request.asGet, response.clone());
     } catch (error) {
       console.debug("Error accessing cache :>> ", error);
       return null;
@@ -52,7 +46,7 @@ class Cache {
   async delete(request, options) {
     try {
       const cache = await this._open();
-      return cache && cache.delete(request, options);
+      return cache.delete(request.asGet, options);
     } catch (error) {
       console.debug("Error accessing cache :>> ", error);
       return null;
@@ -60,9 +54,7 @@ class Cache {
   }
 
   async purge() {
-    if (prefs.get("disable_cache")) {
-      return null;
-    }
+    if (prefs.get("disable_cache")) return null;
     try {
       const keys = await window.caches.keys();
       return Promise.all(keys.map(key => window.caches.delete(key)));
@@ -73,9 +65,7 @@ class Cache {
   }
 
   async update() {
-    if (prefs.get("disable_cache")) {
-      return null;
-    }
+    if (prefs.get("disable_cache")) return null;
     try {
       // Remove caches for old versions
       const keys = await window.caches.keys();
@@ -88,6 +78,55 @@ class Cache {
   }
 }
 
-export const cache = new Cache({ version: "1" });
+/**
+ * POST requests are not cacheable, and GET requests are not allowed to have a
+ * body. So we convert POST requests to GET requests for the purpose of caching.
+ * The real POST request is sent to the server, but within the cache it's keyed
+ * by the GET format.
+ */
+export class CacheableRequest extends Request {
+  /**
+   * Same as `new Request(input, options)` but with a method to convert to GET.
+   * @param {string|Request} input e.g. "/api/bugs"
+   * @param {object} options RequestInit options - important ones:
+   * @param {HeadersInit} [options.headers] Headers for the request
+   * @param {BodyInit} [options.body] e.g. a JSON string
+   * @param {string} [options.method] e.g. "POST"
+   */
+  constructor(
+    input,
+    {
+      body,
+      method = "POST",
+      headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      ...options
+    } = {}
+  ) {
+    super(input, { ...options, body, method, headers });
+    this.path = input;
+    this.body = body;
+  }
 
-module.exports.cache = cache;
+  /**
+   * Return this request, converted to a fake GET request, so it can be cached
+   * (returns itself if it's already a GET request).
+   * @returns {Request|this} A GET request with the same path as the original
+   *                         request, but with the body converted to a query
+   * @example $ new CacheableRequest("/api/bugs", { body: { id: 1 } }).asGet;
+   *          > new Request(`/api/bugs?fakebody={"id":1}`);
+   */
+  get asGet() {
+    if (this.method === "GET") return this;
+    return new Request(`${this.path}?fakebody=${this.body}`);
+  }
+}
+
+export const cache = new BugsCache({ version: "1" });
+
+module.exports = {
+  CacheableRequest,
+  cache,
+};
