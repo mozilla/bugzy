075c14dbbdaeca35e1d33989b676fb17c0934f07
Russell Chan <rchan@mozilla.com>
2023-02-07 14:45:20 -0500

    Restore useBugFetcher.ts changes

--- src/content/hooks/useBugFetcher.ts
+++ src/content/hooks/useBugFetcher.ts
@@ -73,13 +73,12 @@ export function useBugFetcher(
     qm.runCachedQueries(
       query,
       () => isMounted,
-      ({ rsp: { bugs }, awaitingNetwork }: BugQueryReturn) => {
+      ({ rsp: { bugs }, awaitingNetwork }: BugQueryReturn) =>
         setState({
           bugs: transformBugs ? transformBugs(bugs) : bugs,
           status: "loaded",
           awaitingNetwork,
-        });
-      }
+        })
     );
     return () => {
       isMounted = false;
