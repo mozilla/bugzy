77f1de4ef47f8219075fa369f52237963f6212db
Russell Chan <rchan@mozilla.com>
2023-02-07 17:02:55 -0500

    fix: use default values in prefs/store system

--- src/config/pref_defaults.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // The user's bugzilla email
-  bugzilla_email: "",
-  // Disable all client-side network requests
-  offline_debug: false,
-  // Disable the user cache for bug views
-  disable_cache: false,
-};
