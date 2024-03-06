a87c80dddff1e108c8b17430552f0e4e999a8858
Shane Hughes <shughes@mozilla.com>
2024-03-05 21:14:20 -0800

    cleanup: old unused vars in project_settings.js

--- src/config/project_settings.js
+++ src/config/project_settings.js
@@ -9,14 +9,8 @@ export const BUGZILLA_GENERAL_TRIAGE_COMPONENTS = [
 ];
 // What is the bug number of the epic containing all meta bugs as blockers?
 export const EPIC_BUG_NUMBER = "1534999";
-// Old epic bug blocking <2019 projects/features
-export const LEGACY_EPIC_BUG_NUMBER = "1433315";
 // Component to file new feature metabugs under (see All Features page)
 export const FILE_NEW_BUGZILLA_COMPONENT = "Messaging System";
-// Metabug for triaging pocket bugs
-export const POCKET_META = 1535711;
-// Used for some descriptions / titles
-export const PROJECT_NAME = "Activity Stream";
 // Document describing release information
 export const RELEASE_DOC_LINK =
   "https://docs.google.com/spreadsheets/d/1OTNN20IhUm_sPq6awL6cqFTShi4kqCGn6IRvQBL-bcQ";
